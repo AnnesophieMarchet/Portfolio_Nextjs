9abab56da78d8375b300157e14bc4fd8196be1ed
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-03 11:37:37 +0200

    icons section Header.tsx

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,20 +1,41 @@
 import React from "react";
 import { Sections } from "./Sections";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { GithubIcon } from "./icons/GithubIcon";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { LinkedinIcon } from "./icons/LinkedinIcon";
+import { EmailIcon } from "./icons/EmailIcon";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
-      <Sections className="flex item-baseline">
-        <h1 className="text-lg font-bold text-primary-foreground">
+    <header className="sticky top-0 py-4">
+      <Sections className="flex item-baseline ">
+        <h1 className="text-lg font-bold text-primary">
           https://annesophiemarchet.github.io/
         </h1>
         <div className="flex-1 " />
-        <ul>
-          <Button className="size-6">
-            <GithubIcon size={16} className="text-foreground" />
-          </Button>
+        <ul className="flex items-center gap-2">
+          <Link
+            href="https://github.com/AnnesophieMarchet"
+            target="_blank"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <GithubIcon size={12} className="text-foreground" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/anne-sophie-marchet-3a9a58269/"
+            target="_blank"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <LinkedinIcon size={12} className="text-foreground" />
+          </Link>
+          <Link
+            href="mailto:annesophiemarchet@gmail.com"
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+          >
+            <EmailIcon size={12} className="text-foreground" />
+          </Link>
         </ul>
       </Sections>
     </header>
