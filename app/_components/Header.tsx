90c03001603dbc90c007310e11aaf2b7f653ec4c
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-07 12:59:51 +0200

    style general + content Hero.tsx

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,35 +9,46 @@ import { EmailIcon } from "./icons/EmailIcon";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 py-4">
-      <Section className="flex item-baseline ">
-        <h1 className="text-lg font-bold text-primary">
-          https://annesophiemarchet.github.io/
-        </h1>
-        <div className="flex-1 " />
-        <ul className="flex items-center gap-2">
-          <Link
-            href="https://github.com/AnnesophieMarchet"
-            target="_blank"
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-          >
-            <GithubIcon size={12} className="text-foreground" />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/anne-sophie-marchet-3a9a58269/"
-            target="_blank"
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-          >
-            <LinkedinIcon size={12} className="text-foreground" />
-          </Link>
-          <Link
-            href="mailto:annesophiemarchet@gmail.com"
-            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
-          >
-            <EmailIcon size={12} className="text-foreground" />
-          </Link>
-        </ul>
-      </Section>
-    </header>
+    <>
+      <header className="sticky top-0 py-4">
+        <Section className="flex item-baseline ">
+          <h1 className="text-lg font-bold text-primary">
+            www.annesophiemarchet.github.io
+          </h1>
+          <div className="flex-1 " />
+          <ul className="flex items-center gap-4">
+            <Link
+              href="https://github.com/AnnesophieMarchet"
+              target="_blank"
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "size-6 p-0"
+              )}
+            >
+              <GithubIcon size={18} className="text-foreground" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/anne-sophie-marchet-3a9a58269/"
+              target="_blank"
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "size-6 p-0"
+              )}
+            >
+              <LinkedinIcon size={18} className="text-foreground" />
+            </Link>
+            <Link
+              href="mailto:annesophiemarchet@gmail.com"
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "size-6 p-0"
+              )}
+            >
+              <EmailIcon size={18} className="text-foreground" />
+            </Link>
+          </ul>
+        </Section>
+      </header>
+    </>
   );
 };
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,43 +5,55 @@ import Link from "next/link";
 import { Code } from "./Code";
 
 export const Hero = () => {
+  const contenu = [
+    {
+      texte:
+        "Après une carrière dans l'industrie pharmaceutique et chimique en tant que technicienne de fabrication, je me suis lancée dans le monde du développement web. Passionnée de technologie et avide d'apprendre, j'ai suivi des formations en ligne sur Codecademy (JavaScript, CSS, PHP, SQL) ainsi qu'un Bootcamp intensif de 6 mois à la Wild Code School de Lyon (Tailwind, React, Next.js, Express.js). Mes projets sont visibles sur ",
+      lien: "https://github.com/AnnesophieMarchet",
+      icone: <GitHubLogoIcon className="inline" />,
+    },
+    {
+      texte:
+        ". Je suis désormais à la recherche d'une opportunité à partir de septembre 2024 pour enrichir mon expérience et renforcer mes compétences dans le domaine du développement web. Vous trouverez plus de détails sur mon parcours sur ",
+      lien: "https://www.linkedin.com/in/anne-sophie-marchet-3a9a58269/",
+      icone: <LinkedInLogoIcon className="inline" />,
+    },
+    {
+      texte: "N'hésitez pas à me contacter.",
+    },
+  ];
+
+  const texte3 = ". N'hésitez pas à me contacter.";
+
   return (
-    <Section className="flex max-md:flex-col items-start px-4  gap-4">
+    <Section className="flex max-md:flex-col items-start px-4 gap-4">
       <div className="flex-[3] flex flex-col w-full gap-2">
         <h2 className="font-caption text-5xl text-primary">
           Anne-sophie Marchet
         </h2>
         <h3 className="text-3xl font-caption">Développeuse Web Full-Stack</h3>
         <p className="text-base">
-          Mes projets son visible sur{" "}
-          <Link href="https://github.com/AnnesophieMarchet" target="_blank">
-            <Code className="inline-flex items-center gap-1">
-              <GitHubLogoIcon className="inline" />
-              Github
-            </Code>
-          </Link>
-          . Raconte ta vie Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Dolor itaque alias quidem! Odit, quam maiores. Mon parcours est
-          disponible sur{" "}
-          <Link
-            href="https://www.linkedin.com/in/anne-sophie-marchet-3a9a58269/"
-            target="_blank"
-          >
-            <Code className="inline-flex items-center gap-1">
-              <LinkedInLogoIcon className="inline" />
-              Linkedin
-            </Code>
-          </Link>
-          Mollitia fugit dolorem rem corporis eos adipisci reprehenderit quis,
-          doloremque nemo amet distinctio vero provident animi recusandae
-          facilis quod autem optio praesentium assumenda voluptates? Dolores!
+          {contenu.map((item, index) => (
+            <span key={index}>
+              {item.texte}
+              {item.lien && (
+                <Link href={item.lien} target="_blank">
+                  <Code className="inline-flex items-center gap-1">
+                    {item.icone}
+                    {index === 0 ? "Github" : "Linkedin"}
+                  </Code>
+                </Link>
+              )}
+            </span>
+          ))}
+          {texte3}
         </p>
       </div>
       <div className="flex-[2] max-md:m-auto ml-auto">
         <img
           src="https://annesophiemarchet.github.io/assets/Photo%20CV.png"
-          alt="picture anne sophie"
-          className="w-full h-auto max-w-xs rounded-sm max-md:w-56 "
+          alt="photo anne sophie"
+          className="w-full h-auto max-w-xs rounded-sm max-md:w-56"
         />
       </div>
     </Section>
